6a2571df4babd95dfad15b5c05f0cb2b5f8ad5d7
John Mark Harrell <hijohnmark@gmail.com>
2024-01-05 13:06:37 -0800

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let ntd = document.getElementById('new-task-description')
+    buildToDo(ntd.value)
+    form.reset()
+
+  })
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  
+  let dropDown = document.createElement('select')
+  let optionA = document.createElement('option')
+  let optionB = document.createElement('option')
+  let optionC = document.createElement('option')
+  optionA.textContent = "high priority"
+  optionB.textContent = "medium priority"
+  optionC.textContent = "low priority"
+
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "X"
+  p.textContent = `${todo} `
+  p.appendChild(dropDown)
+  dropDown.appendChild(optionA)
+  dropDown.appendChild(optionB)
+  dropDown.appendChild(optionC)
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
